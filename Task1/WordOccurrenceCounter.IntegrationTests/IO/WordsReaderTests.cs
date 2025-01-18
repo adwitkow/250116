58f1d429a1bdbc52e14b327b713bd1c749aaa8c2
adwitkow <adamadik@poczta.fm>
2025-01-18 20:22:25 +0100

    WordOccurrenceCounter: Implement WordsReader

---
+++ Task1/WordOccurrenceCounter.IntegrationTests/IO/WordsReaderTests.cs
@@ -0,0 +1,116 @@
+﻿using Bogus;
+using WordOccurrenceCounter.IO;
+
+namespace WordOccurrenceCounter.IntegrationTests.IO;
+
+[TestFixture]
+[Category("Integration")]
+public class WordsReaderTests
+{
+    private const int Seed = 42;
+    private const string TestDataDirectory = "TestData";
+
+    private static readonly string Oneliner1Path = Path.Combine(TestDataDirectory, "Oneliner1.txt");
+    private static readonly string Oneliner2Path = Path.Combine(TestDataDirectory, "Oneliner2.txt");
+    private static readonly string OnelinerSpecialPath = Path.Combine(TestDataDirectory, "OnelinerSpecial.txt");
+    private static readonly string LongOnelinerPath = Path.Combine(TestDataDirectory, "LongOneliner.txt");
+    private static readonly string MultipleLinesPath = Path.Combine(TestDataDirectory, "MultipleLines.txt");
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        var faker = new Faker();
+        faker.Random = new Randomizer(Seed);
+
+        // 10 million words of lorem ipsum,
+        // beginning with a capital letter and ending with a dot.
+        var sentence = faker.Lorem.Sentence(10_000_000);
+        File.WriteAllText(LongOnelinerPath, sentence);
+
+        // 1k short sentences, each in its own line,
+        // formatted in the same manner as above
+        var sentences = faker.Lorem.Sentences(1_000);
+        File.WriteAllText(MultipleLinesPath, sentences);
+    }
+
+    [OneTimeTearDown]
+    public void OneTimeTearDown()
+    {
+        if (File.Exists(LongOnelinerPath))
+        {
+            File.Delete(LongOnelinerPath);
+        }
+
+        if (File.Exists(MultipleLinesPath))
+        {
+            File.Delete(MultipleLinesPath);
+        }
+    }
+
+    [Test]
+    public void ReadWords_OneLiner1()
+    {
+        using var reader = new WordsReader(Oneliner1Path);
+        
+        var words = reader.ReadWords().ToArray();
+
+        var expected = new[] { "Go", "do", "that", "thing", "that", "you", "do", "so", "well" };
+        Assert.That(words, Is.EquivalentTo(expected));
+    }
+
+    [Test]
+    public void ReadWords_OneLiner2()
+    {
+        using var reader = new WordsReader(Oneliner2Path);
+
+        var words = reader.ReadWords().ToArray();
+
+        var expected = new[] { "I", "play", "football", "well" };
+        Assert.That(words, Is.EquivalentTo(expected));
+    }
+
+    [Test]
+    public void ReadWords_OneLinerWithSpecialCharacters()
+    {
+        using var reader = new WordsReader(OnelinerSpecialPath);
+
+        var words = reader.ReadWords().ToArray();
+
+        var expected = new[] { "Lorem", "ipsum", "dolor", "sit", "amet" };
+        Assert.That(words, Is.EquivalentTo(expected));
+    }
+
+    [Test]
+    public void ReadWords_LongOneliner()
+    {
+        using var reader = new WordsReader(LongOnelinerPath);
+
+        var words = reader.ReadWords().ToArray();
+
+        int expectedCount = 10_000_000;
+        Assert.Multiple(() =>
+        {
+            Assert.That(words[0], Is.EqualTo("Libero"));
+            Assert.That(words[^1], Is.EqualTo("voluptatem"));
+            Assert.That(words, Has.Length.EqualTo(expectedCount));
+        });
+    }
+
+    [Test]
+    public void ReadWords_MultipleLines()
+    {
+        using var reader = new WordsReader(MultipleLinesPath);
+
+        var words = reader.ReadWords().ToArray();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(words[0], Is.EquivalentTo("Soluta"));
+            Assert.That(words[1000], Is.EquivalentTo("Animi"));
+            Assert.That(words[3000], Is.EquivalentTo("ut"));
+            Assert.That(words[6000], Is.EquivalentTo("vel"));
+            Assert.That(words[^1], Is.EquivalentTo("porro"));
+            Assert.That(words, Has.Length.EqualTo(6402));
+        });
+    }
+}
---
+++ Task1/WordOccurrenceCounter/IO/WordsReader.cs
@@ -0,0 +1,110 @@
+﻿namespace WordOccurrenceCounter.IO;
+
+public class WordsReader : IDisposable
+{
+    private const int DefaultBufferSize = 8192; // 8 KB feels pretty good
+
+    private readonly StreamReader _reader;
+    private readonly char[] _buffer;
+    private string? _leftover;
+
+    private bool disposedValue;
+
+    public WordsReader(string path) : this(path, DefaultBufferSize)
+    {
+    }
+
+    public WordsReader(string path, int bufferSize)
+    {
+        if (string.IsNullOrWhiteSpace(path))
+        {
+            throw new ArgumentException("File path cannot be null or empty.", nameof(path));
+        }
+
+        if (bufferSize <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(bufferSize), "Buffer size must be greater than zero.");
+        }
+
+        _reader = new StreamReader(path);
+        _buffer = new char[bufferSize];
+        _leftover = null;
+    }
+
+    public IEnumerable<string> ReadWords()
+    {
+        int charsRead;
+
+        while ((charsRead = _reader.Read(_buffer, 0, _buffer.Length)) > 0)
+        {
+            int wordStartIndex = 0;
+
+            for (int i = 0; i < charsRead; i++)
+            {
+                if (!char.IsLetter(_buffer[i]))
+                {
+                    if (i > wordStartIndex)
+                    {
+                        string word = ExtractWord(wordStartIndex, i);
+                        yield return word;
+                    }
+                    else if (_leftover is not null)
+                    {
+                        yield return _leftover;
+                        _leftover = string.Empty;
+                    }
+
+                    wordStartIndex = i + 1;
+                }
+            }
+
+            if (wordStartIndex < charsRead)
+            {
+                _leftover += new string(_buffer, wordStartIndex, charsRead - wordStartIndex);
+            }
+        }
+
+        if (!string.IsNullOrEmpty(_leftover))
+        {
+            yield return _leftover;
+            _leftover = null;
+        }
+    }
+
+    private string ExtractWord(int startIndex, int endIndex)
+    {
+        string word = _leftover + new string(_buffer, startIndex, endIndex - startIndex);
+        _leftover = null;
+
+        return word;
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                _reader?.Dispose();
+            }
+
+            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+            // TODO: set large fields to null
+            disposedValue = true;
+        }
+    }
+
+    // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+    // ~WordReader()
+    // {
+    //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+    //     Dispose(disposing: false);
+    // }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
