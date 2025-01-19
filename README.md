da96699fd1d8c9a4fe87529b75127b24216c2715
adwitkow <adamadik@poczta.fm>
2025-01-19 17:30:23 +0100

    WordOccurrenceCounter: Add benchmarks

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-# TriangleChecker
+# 1. WordOccurrenceCounter
+```
+Usage: WordOccurrenceCounter.Console [--files <String>...] [--help] [--version]
+
+E.g: WordOccurrenceCounter.Console -f ..\..\..\..\WordOccurrenceCounter.IntegrationTests\TestData\Oneliner1.txt -f ..\..\..\..\WordOccurrenceCounter.IntegrationTests\TestData\Oneliner2.txt
+
+WordOccurrenceCounter.Console
+
+Options:
+  -f, --files <String>...     (Required)
+  -h, --help                 Show help message
+  --version                  Show version
+```
+
+## File reading benchmarks
+```
+BenchmarkDotNet v0.14.0, Windows 10 (10.0.19045.5131/22H2/2022Update)
+12th Gen Intel Core i5-12600K, 1 CPU, 16 logical and 10 physical cores
+.NET SDK 9.0.101
+  [Host]     : .NET 8.0.11 (8.0.1124.51707), X64 RyuJIT AVX2
+  DefaultJob : .NET 8.0.11 (8.0.1124.51707), X64 RyuJIT AVX2
+```
+
+| Method         | FileSize | Mean         | Error      | StdDev     | Gen0         | Gen1         | Gen2         | Allocated   |
+|--------------- |--------- |-------------:|-----------:|-----------:|-------------:|-------------:|-------------:|------------:|
+| ReadAllLines   | large    | 19,307.65 ms | 145.925 ms | 136.499 ms | 1657000.0000 |  623000.0000 |   11000.0000 | 19540.61 MB |
+| StreamReader   | large    | 17,263.46 ms | 146.790 ms | 137.308 ms | 2105000.0000 | 1428000.0000 | 1005000.0000 | 19539.92 MB |
+| BufferedStream | large    | 17,048.05 ms | 137.324 ms | 128.453 ms | 2097000.0000 | 1422000.0000 | 1005000.0000 | 19539.92 MB |
+| WordsReader    | large    |  7,145.76 ms |  67.731 ms |  63.356 ms |  876000.0000 |            - |            - |  8739.05 MB |
+| ReadAllLines   | small    |     41.50 ms |   0.803 ms |   1.073 ms |    4166.6667 |    2833.3333 |    1000.0000 |    39.17 MB |
+| StreamReader   | small    |     37.17 ms |   0.698 ms |   0.653 ms |    4428.5714 |    2500.0000 |    2214.2857 |    39.16 MB |
+| BufferedStream | small    |     36.16 ms |   0.722 ms |   0.859 ms |    4428.5714 |    2500.0000 |    2214.2857 |    39.17 MB |
+| WordsReader    | small    |     14.55 ms |   0.247 ms |   0.231 ms |    1765.6250 |      78.1250 |            - |    17.61 MB |
+
+# 2. TriangleChecker
 ```
 Usage: TriangleChecker.Console [--side a <Double>] [--side b <Double>] [--side c <Double>] [--help] [--version]
 
---
+++ Task1/WordOccurrenceCounter.Benchmarks/Program.cs
@@ -0,0 +1,106 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Running;
+using Bogus;
+using System.Text.RegularExpressions;
+using WordOccurrenceCounter.IO;
+
+BenchmarkRunner.Run<Benchmarks>();
+
+[MemoryDiagnoser]
+public class Benchmarks
+{
+    private const string FilePath = "testfile.txt";
+
+    private Consumer _consumer = new Consumer();
+
+    [Params("small", "large")]
+    public string FileSize { get; set; }
+
+    private int Size => FileSize == "small"
+        ? 100
+        : 50_000;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        var faker = new Faker();
+        faker.Random = new Randomizer(42);
+
+        if (File.Exists(FilePath))
+        {
+            File.Delete(FilePath);
+        }
+
+        using var writer = new StreamWriter(FilePath);
+        for (int i = 0; i < Size; i++)
+        {
+            writer.WriteLine(faker.Lorem.Sentence(10, 10_000));
+        }
+    }
+
+    [GlobalCleanup]
+    public void Cleanup()
+    {
+        if (File.Exists(FilePath))
+        {
+            File.Delete(FilePath);
+        }
+    }
+
+    [Benchmark]
+    public void ReadAllLines()
+    {
+        var lines = File.ReadAllLines(FilePath);
+        foreach (var line in lines)
+        {
+            var words = Regex.Split(line, @"\W");
+            foreach (var word in words)
+            {
+                _consumer.Consume(word);
+            }
+        }
+    }
+
+    [Benchmark]
+    public void StreamReader()
+    {
+        using var reader = new StreamReader(FilePath);
+        string? line;
+        while ((line = reader.ReadLine()) != null)
+        {
+            var words = Regex.Split(line, @"\W");
+            foreach (var word in words)
+            {
+                _consumer.Consume(word);
+            }
+        }
+    }
+
+    [Benchmark]
+    public void BufferedStream()
+    {
+        using var stream = new FileStream(FilePath, FileMode.Open, FileAccess.Read, FileShare.Read, 8192, FileOptions.SequentialScan);
+        using var bufferedStream = new BufferedStream(stream);
+        using var reader = new StreamReader(bufferedStream);
+        string? line;
+        while ((line = reader.ReadLine()) != null)
+        {
+            var words = Regex.Split(line, @"\W");
+            foreach (var word in words)
+            {
+                _consumer.Consume(word);
+            }
+        }
+    }
+
+    [Benchmark]
+    public void WordsReader()
+    {
+        using var reader = new WordsReader(FilePath);
+        foreach (var word in reader.ReadWords())
+        {
+            _consumer.Consume(word);
+        }
+    }
+}
